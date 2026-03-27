f5e250d3ab37ff2c0b00a6d64f99c4d0096957ff
mugbya <mugbya@gmail.com>
2026-03-27 17:17:33 +0800

    style:去掉阴影样式

--- Selene-mysql/src/components/common/dialog/ConnectionFormDialog.tsx
+++ Selene-mysql/src/components/common/dialog/ConnectionFormDialog.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { SiMysql } from 'react-icons/si';
 import { DBConnectionPersisted, DBConnectionRuntime } from '@/types';
 import { Input } from '@/components/ui/input';
+import { Eye, EyeOff } from 'lucide-react';
 
 interface Props {
   open: boolean;
@@ -30,6 +31,8 @@ export default function ConnectionFormDialog({
     databases: [],
   });
 
+
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     if (defaultValue) {
@@ -67,7 +70,22 @@ export default function ConnectionFormDialog({
               <Input name="port" type="number" value={form.port} onChange={handleChange} placeholder="Port" className="w-full" />
             </div>
             <Input name="username" value={form.username} onChange={handleChange} placeholder="用户名" className="w-full" />
-            <Input name="password" value={form.password} onChange={handleChange} placeholder="密码" className="w-full" />
+            <div className="relative">
+              <Input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                placeholder="密码"
+                className="w-full pr-6"
+              />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+              >
+                {showPassword ? <EyeOff className="w-4 h-4" /> : <Eye className="w-4 h-4" />}
+              </span>
+            </div>
           </div>
         </div>
 
